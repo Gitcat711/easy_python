0d064d1539e62b00e88c9b44444f2091dce152f4
Pratik2799 <pithore.pratik@gmail.com>
2018-03-03 19:05:11 +0530

    added more examples

---
+++ conditional-statement/conditional-statement.md
@@ -0,0 +1,5 @@
+## if..else..elif
+* The if..else..elif function is used in python for decision making.
+  * The elif function is used when there are more than two condition.
+    * We can add if..else..elif statement inside another if..else..elif statement this is called nesting.
+  
---
+++ conditional-statement/if..else..elif.py
@@ -0,0 +1,8 @@
+#we will find whether the number is positive or negative
+a=int(input("enter the number="))
+if a<0:
+    print('number is negative')
+elif a>0: #if there are more than two condition 'elif' can be used
+    print("number is positive")
+else: #
+    print("number is zero")
