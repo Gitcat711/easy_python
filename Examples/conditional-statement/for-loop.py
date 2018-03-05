3154eaa34b1fbfa53cf217eda97c7c313257fa93
Pratik2799 <pithore.pratik@gmail.com>
2018-03-05 18:52:37 +0530

    added new programms and some changes in folder

---
+++ Examples/conditional-statement/for-loop.py
@@ -0,0 +1,6 @@
+# program to find the sum of all numbers present in list(you can edit the values of list)
+a = [5,6,7,8,21,9,11,41] # list is always written in square bracket.
+sum = 0 # variable to  store the sum.
+for i in a:# here the loop is applied to repeat over list.
+    sum =sum+i
+print("The sum is",sum)#this will print the sum of numbers present in the list
---
+++ Examples/conditional-statement/if..else..elif.py
@@ -0,0 +1,8 @@
+# we will find out whether the number is positive or negative.
+a = int(input("Enter the number="))
+if a<0:#here the number will be checked whether the it is less than zero.
+    print("The number is negative")#if the above conditon satisfies it will the output.
+elif  a>0:# elif is used when there is more than two condition.
+    print("The number is positive")
+else:#if above conditions are not satisfied then else function will break that and print the output zero.
+    print("The number is zero")
